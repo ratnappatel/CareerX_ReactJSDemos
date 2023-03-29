136c3e4056c8eef40e85ad46caa3cd25ca8eddd2
ratnappatel <ratnappatel@gmail.com>
2023-03-29 20:03:43 +0530

    Express CRUD Example

---
+++ CRUD-Express/server.js
@@ -0,0 +1,79 @@
+// Main entry point of our web-application
+
+const express=require('express');
+const bodyParser=require('body-parser');
+// Driver module for Mongodb Connec support
+const mongoose=require('mongoose');
+
+//Create Definition of an Object[MongoDB Document] which is getting retrieved from DB
+const ItemSchema=mongoose.Schema({
+    item: String,
+    price: Number,
+    quantity:Number,
+    date: Date
+});
+
+const item=mongoose.model('Item',ItemSchema);
+
+//Creating New WebApplication as Middleware
+const app=express();
+
+// Connect to MongoDB Database
+mongoose.connect('mongodb://localhost:27017/groceryDB',{useNewUrlParser:true})
+.then(()=>{
+    console.log('DB Connected Successfully.')
+})
+.catch(()=>{
+    console.log('Could not Connect to DB..');
+    process.exit();
+
+})
+
+//extended=true==> accept data as Array/Objects only
+app.use(bodyParser.urlencoded({extended:true}))
+
+//parse requests of content-type = application/json
+app.use(bodyParser.json())
+
+app.get('/',(req,res)=>{
+    res.json({"message": 'Welcome to ToDo Application Development'});
+})
+
+//Get All Item ==> Read
+app.get('/items',(req,res)=>{
+    item.find().then(items=>{
+        res.send(items);
+    }).catch(err=>{
+        res.status(500).send({message:"Data Could not retrieved.."})
+    })
+})
+
+
+// Add new Item  ==> Create
+app.post('/items',(req,res)=>{
+    console.log(req.body.content);
+    if(!req.body.content)
+        return res.status(400).send({
+            message:"Item Content are missing"
+        });
+        const data=new item({
+            item:req.body.item ||'Undefined Item',
+            price:req.body.price || 0,
+            quantity:req.body.quantity|| 0,
+            date:req.body.date || new Date()
+        })
+        data.save()
+        .then(d=>{
+            res.send(d);
+        })
+        .catch(err=>{
+            res.status(500).send({
+                message:"Could not save Item.."
+            })
+        });
+})
+
+
+app.listen(3004,()=>{
+    console.log('Server is Listening on port 3004');
+})
